eed9a82709b6ab300594d9bd2a5dbbcf0c76a493
lirelent <smarts40@gmail.com>
2013-02-22 08:57:08 -0500

    add LGPLv3 license and start on oil fill level indicator for oil generator. Still need to work out how to get it to fill from bottom up correctly.

--- src/minecraft/oilcraft/generators/OilGeneratorGuiContainer.java
+++ src/minecraft/oilcraft/generators/OilGeneratorGuiContainer.java
@@ -1,3 +1,21 @@
+/*
+ *  Copyright 2013 Ryan Metzger
+ *
+ *  This file is part of OilCraft.
+ *
+ *  OilCraft is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  OilCraft is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with OilCraft.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package oilcraft.generators;
 
 import net.minecraft.client.gui.inventory.GuiContainer;
@@ -11,47 +29,81 @@ import oilcraft.Oilcraft;
 
 import org.lwjgl.opengl.GL11;
 
+import cpw.mods.fml.common.FMLLog;
+
 public class OilGeneratorGuiContainer extends GuiContainer
 {
 
-	public OilGeneratorGuiContainer(InventoryPlayer IP, OilGeneratorTileEntity tileEntity)
-	{
-		super(new OilGeneratorContainer(IP, tileEntity));
-	}
+    public OilGeneratorGuiContainer(InventoryPlayer IP,
+	    OilGeneratorTileEntity tileEntity)
+    {
+	super(new OilGeneratorContainer(IP, tileEntity));
+    }
 
-	protected void drawGuiContainerForegroundLayer()
-	{
-		// draw text and stuff here
-		// the parameters for drawString are: string, x, y, color
-		fontRenderer.drawString("Oil Generator", 8, 6, 4210752);
-		// draws "Inventory" or your regional equivalent
-		fontRenderer.drawString(StatCollector.translateToLocal("container.inventory"), 8, ySize - 96 + 2, 4210752);
-	}
+    protected void drawGuiContainerForegroundLayer()
+    {
+	// draw text and stuff here
+	// the parameters for drawString are: string, x, y, color
+	fontRenderer.drawString("Oil Generator", 8, 6, 4210752);
+	// draws "Inventory" or your regional equivalent
+	fontRenderer.drawString(
+		StatCollector.translateToLocal("container.inventory"), 8,
+		ySize - 96 + 2, 4210752);
+    }
 
-	@Override
-	protected void drawGuiContainerBackgroundLayer(float par1, int par2, int par3)
-	{
-		// draw your Gui here, only thing you need to change is the path
-		int texture = mc.renderEngine.getTexture(Oilcraft.TEXTURE_PATH + "OilGenerator.png");
-		GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
-		this.mc.renderEngine.bindTexture(texture);
-		int x = (width - xSize) / 2;
-		int y = (height - ySize) / 2;
-		this.drawTexturedModalRect(x, y, 0, 0, xSize, ySize);
-	}
+    @Override
+    protected void drawGuiContainerBackgroundLayer(float par1,
+	    int par2,
+	    int par3)
+    {
+	// draw your Gui here, only thing you need to change is the path
+	int texture = mc.renderEngine.getTexture(Oilcraft.TEXTURE_PATH
+		+ "OilGenerator.png");
+	GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
+	this.mc.renderEngine.bindTexture(texture);
+	int x = (width - xSize) / 2;
+	int y = (height - ySize) / 2;
+	this.drawTexturedModalRect(x, y, 0, 0, xSize, ySize);
 
-	public Object getServerGuiElement(int id, EntityPlayer player, World world, int x, int y, int z)
-	{
-		TileEntity tileEntity = world.getBlockTileEntity(x, y, z);
-		if (tileEntity instanceof OilGeneratorTileEntity) { return new OilGeneratorContainer(player.inventory, (OilGeneratorTileEntity) tileEntity); }
-		return null;
-	}
+	/*
+	 * top of tank is 4 px down and bottom is 72 px down
+	 */
+	int emptyPixels = (int) ((72 - 4) * (1 - 0.25));
+	FMLLog.warning("Empty pixels is "+emptyPixels);
+	//this.drawTexturedModalRect(x + 5, y + 4, 177, 1, 26, 3 + fillPixels);
+	this.drawTexturedModalRect(x + 5, y + 4 + 4 + emptyPixels, 177, 72 + 4 + emptyPixels, 26, 4 + ((72-4) - emptyPixels));
+    }
 
-	public Object getClientGuiElement(int id, EntityPlayer player, World world, int x, int y, int z)
+    public Object getServerGuiElement(int id,
+	    EntityPlayer player,
+	    World world,
+	    int x,
+	    int y,
+	    int z)
+    {
+	TileEntity tileEntity = world.getBlockTileEntity(x, y, z);
+	if (tileEntity instanceof OilGeneratorTileEntity)
 	{
-		TileEntity tileEntity = world.getBlockTileEntity(x, y, z);
-		if (tileEntity instanceof OilGeneratorTileEntity) { return new OilGeneratorGuiContainer(player.inventory, (OilGeneratorTileEntity) tileEntity); }
-		return null;
+	    return new OilGeneratorContainer(player.inventory,
+		    (OilGeneratorTileEntity) tileEntity);
 	}
+	return null;
+    }
+
+    public Object getClientGuiElement(int id,
+	    EntityPlayer player,
+	    World world,
+	    int x,
+	    int y,
+	    int z)
+    {
+	TileEntity tileEntity = world.getBlockTileEntity(x, y, z);
+	if (tileEntity instanceof OilGeneratorTileEntity)
+	{
+	    return new OilGeneratorGuiContainer(player.inventory,
+		    (OilGeneratorTileEntity) tileEntity);
+	}
+	return null;
+    }
 
 }
